37a552a7e4e47a54aaed9076db4bd2293576bc52
unknown <Proyectos@D27-015.upc.edu.pe>
2023-11-21 07:45:33 -0500

    service completos

--- src/main/java/com/mascotitas/TF/Service/VeterinariaService.java
+++ src/main/java/com/mascotitas/TF/Service/VeterinariaService.java
@@ -4,9 +4,11 @@ import com.mascotitas.TF.Repository.VeterinariaRepository;
 import com.mascotitas.TF.entity.Veterinaria;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +17,36 @@ public class VeterinariaService {
     private final VeterinariaRepository veterinariaRepository;
 
     @Transactional
-    public Veterinaria createVeterinaria(Veterinaria vete)
-    {
+    public Veterinaria createVeterinaria(Veterinaria vete) {
         return veterinariaRepository.save(vete);
     }
-    public List<Veterinaria> getAllVeterinaria()
-    {
+
+    @Transactional
+    public List<Veterinaria> getAllVeterinaria() {
         return veterinariaRepository.findAll();
+    }
+
+    @Transactional
+    public Veterinaria update( Long veterinariaId, Veterinaria veteResource)
+    {
+        Optional<Veterinaria> optionalVeterinaria = veterinariaRepository.findById(veterinariaId);
+        if (optionalVeterinaria.isPresent())
+        {
+            Veterinaria veterinaria = optionalVeterinaria.get();
+
+            veterinaria.setNombre(veteResource.getNombre());
+            veterinaria.setDescripcion(veteResource.getDescripcion());
+            veterinaria.setDireccion(veteResource.getDireccion());
+            veterinaria.setHistorial_medico(veteResource.getHistorial_medico());
+            veterinaria.setVeterinario(veteResource.getVeterinario());
+
+            veterinaria = veterinariaRepository.save(veterinaria);
+            return veterinaria;
+
+        }
+
+        throw new ResourceNotFoundException("Veterinaria not found  with id: " + veterinariaId);
+
     }
 
 }
--- src/main/java/com/mascotitas/TF/Service/VeterinariosService.java
+++ src/main/java/com/mascotitas/TF/Service/VeterinariosService.java
@@ -5,9 +5,11 @@ import com.mascotitas.TF.entity.Citas;
 import com.mascotitas.TF.entity.Veterinarios;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +22,28 @@ public class VeterinariosService {
     {
         return veterinariosRepository.save(veterinarios);
     }
+    @Transactional
     public List<Veterinarios> getAllVeterinarios()
     {
         return veterinariosRepository.findAll();
+    }
+
+    @Transactional
+    public Veterinarios updateVeterinarios( Long veterinariosId, Veterinarios veterinariosResource )
+    {
+        Optional<Veterinarios>  optionalVeterinarios = veterinariosRepository.findById(veterinariosId);
+        if (optionalVeterinarios.isPresent())
+        {
+            Veterinarios veterinarios = optionalVeterinarios.get();
+
+            veterinarios.setNombre(veterinariosResource.getNombre());
+            veterinarios.setApellido(veterinariosResource.getApellido());
+
+            veterinarios = veterinariosRepository.save(veterinarios);
+            return veterinarios;
+        }
+        throw new ResourceNotFoundException("Veterinario not found  with id: " + veterinariosId);
+
     }
 
 }
